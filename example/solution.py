3f471c507f7be3043ada5c907bf28449e691844c
juraj80 <juraj.klucka@gmail.com>
2021-08-01 22:54:31 +0200

    added layover calculation

--- example/solution.py
+++ example/solution.py
@@ -1,55 +1,84 @@
 import csv
 import sys
-from datetime import datetime
+import datetime
 
 #bags = 1
 
 data_file = './example/example.csv'
 ticket_origin = 'WIW'
 ticket_destination = 'RFZ'
-
-
-# import csv file and convert it to the list of dictionaries
-with open(data_file, mode='r') as infile:
-    reader = csv.reader(infile)
-    headings = next(reader)
-    flights = []
-
-    for row in reader:
-        flight = {}
-        for i in range(len(row)):
-            flight[headings[i]] = row[i]
-        flights.append(flight)
-
 route = []
 
+"""Helper function for converting datetime string to the datetime object."""
 
-def flight_search(current_flight_origin, current_flight_destination):
+
+def parseToDate(dateString):
+    return datetime.datetime.strptime(dateString, "%Y-%m-%dT%H:%M:%S")
+
+
+"""Imports a source csv file and converts it to the list of dictionaries.
+"""
+
+
+def importFlightsFromCSV(data_file):
+    flights = []
+    with open(data_file, mode='r') as infile:
+        reader = csv.reader(infile)
+        headings = next(reader)
+        for row in reader:
+            flight = {}
+            flight['flight_no'] = row[0]
+            flight['origin'] = row[1]
+            flight['destination'] = row[2]
+            flight['departure'] = parseToDate(row[3])
+            flight['arrival'] = parseToDate(row[4])
+            flight['base_price'] = float(row[5])
+            flight['bag_price'] = float(row[6])
+            flight['bag_allowed'] = int(row[6])
+            flights.append(flight)
+    return flights
+
+
+"""This function iterates over the list of all flights and searches for the flight combinations from the origin to destination airport. """
+
+
+def flight_search(flight_origin, flight_destination, flights):
     for flight in flights:
-        if flight['origin'] == current_flight_origin and flight['destination'] == current_flight_destination:
+        # the direct flight origin -> destination
+        if flight['origin'] == flight_origin and flight['destination'] == flight_destination and not route:
             route.append(flight)
             flights.remove(flight)
             return
 
-        elif flight['origin'] == current_flight_origin and not route:
+        # the indirect flight to destination
+        if flight['origin'] == flight_origin and flight['destination'] == flight_destination and route:
+            layover = flight['departure'] - route[-1]['arrival']
+            if layover > datetime.timedelta(hours=1) and layover < datetime.timedelta(hours=6):
+                route.append(flight)
+                flights.remove(flight)
+                return
+
+        # the indirect flight from origin
+        elif flight['origin'] == flight_origin and not route:
             route.append(flight)
             flights.remove(flight)
-
             flight_search(flight['destination'],
-                          current_flight_destination)
+                          flight_destination, flights)
 
-        elif flight['origin'] == current_flight_origin:
-
-            route.append(flight)
-            flights.remove(flight)
-
-            flight_search(flight['destination'],
-                          current_flight_destination)
+        # the indirect flight
+        elif flight['origin'] == flight_origin:
+            layover = flight['departure'] - route[-1]['arrival']
+            if layover > datetime.timedelta(hours=1) and layover < datetime.timedelta(hours=6):
+                route.append(flight)
+                flights.remove(flight)
+                flight_search(flight['destination'],
+                              flight_destination, flights)
 
         else:
             route.clear()
             return "No such flight with a given paramaters."
 
 
-flight_search(ticket_origin, ticket_destination)
+flights = importFlightsFromCSV(data_file)
+flight_search(ticket_origin, ticket_destination, flights)
 print(route)
